9bf52e12f37e11a6decf8fe4ced910d8e7a0ab65
biocity93 <90408597+biocity93@users.noreply.github.com>
2022-11-27 15:36:54 -0500

    remove app

--- src/components/cards/ThreeColSlider.js
+++ src/components/cards/ThreeColSlider.js
@@ -8,11 +8,9 @@ import { ReactComponent as ChevronLeftIcon } from "feather-icons/dist/icons/chev
 import { ReactComponent as ChevronRightIcon } from "feather-icons/dist/icons/chevron-right.svg";
 import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-1.svg";
 import { ReactComponent as SvgDecoratorBlob2 } from "../../images/svg-decorator-blob-3.svg";
+import { Container} from "components/misc/Layouts.js";
 
-
-const Container = tw.div`relative`;
 const Content = tw.div`max-w-screen-xl mx-auto py-16 lg:py-20`;
-
 const HeadingWithControl = tw.div`flex flex-col items-center sm:items-stretch sm:flex-row justify-between`;
 const Heading = tw(SectionHeading)``;
 const Controls = tw.div`flex items-center`;
@@ -24,13 +22,11 @@ const ControlButton = styled(PrimaryButtonBase)`
 `;
 const PrevButton = tw(ControlButton)``;
 const NextButton = tw(ControlButton)``;
-
 const CardSlider = styled(Slider)`
-  ${tw`mt-16`}
-  .slick-track { 
-    ${tw`flex`}
+${tw`mt-16`}
+  .slick-track {display: flex !important
   }
-  .slick-slide {
+  .slick-slider {
     ${tw`h-auto justify-center mb-1`}
   }
 `;
@@ -45,7 +41,7 @@ const DecoratorBlob1 = tw(
 const DecoratorBlob2 = tw(
   SvgDecoratorBlob2
 )`-z-10 absolute top-0 left-0 w-48 h-48 transform -translate-x-32 translate-y-full opacity-25`;
-
+const Link = tw.a`inline-block mt-4 text-sm text-blue-500 font-bold cursor-pointer transition duration-300 border-b-2 border-transparent hover:border-blue-500`;
 const TextInfo = tw.div`py-6`;
 const TitleReviewContainer = tw.div`flex flex-col sm:flex-row sm:justify-between sm:items-center`;
 const Title = tw.h5`text-2xl font-bold`;
@@ -59,7 +55,10 @@ export default () => {
   const [sliderRef, setSliderRef] = useState(null);
   const sliderSettings = {
     arrows: false,
+    autoplay: false,
     slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [
       {
         breakpoint: 1280,
@@ -84,29 +83,33 @@ export default () => {
       title: "News",
       description: "",
       dateText: "January 2021",
+      url:""
     },
     {
       imageSrc: "https://images.unsplash.com/photo-1604480132736-44c188fe4d20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1590&q=80&auto=format&fit=crop&h=1024&w=768&q=80",
       title: "Resources",
       description: "test",
       dateText: "October 2020",
+      url:""
     },
     {
       imageSrc: "https://images.unsplash.com/photo-1475137979732-b349acb6b7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80&auto=format&fit=crop&h=1024&w=768&q=80",
       title: "Mental Health",
       description: "desc ",
       dateText: "May 2021",
+      url:""
     },
     {
       imageSrc: "https://images.unsplash.com/photo-1561489396-888724a1543d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=1024&w=768&q=80",
       title: "Psychology Speakers",
       description: "article description",
       dateText: "June 2021",
+      url:""
     },
   ]
 
   return (
-    <Container>
+    <Container >
       <Content>
         <HeadingWithControl>
           <Heading>News and Updates</Heading>
@@ -133,6 +136,7 @@ export default () => {
                 </SecondaryInfoContainer>
                 <Description>{card.description}</Description>
               </TextInfo>
+              <Link href={card.url}>See Article</Link>
             </Card>
           ))}
         </CardSlider>
